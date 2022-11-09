637e9c24fb5a84e16760aa5041c205ad98d43fb1
Pablo Personal <josepablovelazquezmichel@gmail.com>
2022-11-09 14:18:41 -0600

    Move Testing Jenkins

--- TestingJenkins/UnitTest1.cs
+++ TestingJenkins/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace TestingJenkins
         public void Test1()
         {
             Console.WriteLine("----------------------------------------------");
-            Console.WriteLine("Pablos Test");
+            Console.WriteLine("Pablos Tesst");
             Console.WriteLine("----------------------------------------------");
         }
     }
