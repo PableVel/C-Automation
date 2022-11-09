8a780b9641794353024423a9394c2941eb760fb3
Pablo Personal <josepablovelazquezmichel@gmail.com>
2022-11-09 14:13:00 -0600

    Testing jenkins

---
+++ TestJenkins/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestJenkins
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TestingJenkins/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace TestingJenkins
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Pablos Test");
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
